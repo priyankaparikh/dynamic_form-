3df5223b939b29ca55432148ab27f774e846ce95
Priyanka Parikh <priyanka.parikh6@gmail.com>
2020-05-21 21:34:04 -0700

    form

---
+++ README.md
@@ -0,0 +1,11 @@
+to run this program , simply clone the repository and in a python 3 environment 
+
+```pip install -r requirements.txt```
+
+Do this from the root repository 
+
+```python server.py```
+
+Improvements : 
+
+1) we could hook this up to a nosql db because this a dynamic form with the fields changing often. With a sql db we may have to keep adding new columns which may mean having to replicate tables (creating and dropping often)
--- server.py
+++ server.py
@@ -4,12 +4,69 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
+app.secret_key = "ABC"
 
-@app.route('/')
+
+
+fields = [{
+  "tag": "input",
+  "name": "first_name",
+  "type": "text",
+  "human_label": "First Name"
+}, {
+  "tag": "input",
+  "name": "last_name",
+  "type": "text",
+	"human_label": "Last Name"
+}, {
+  "tag": "input",
+  "name": "email",
+  "type": "email",
+  "human_label": "Email Address"
+}, {
+  "tag": "input",
+  "name": "phone_number",
+  "type": "text",
+  "human_label": "Phone Number"
+}, {
+  "tag": "input",
+  "name": "job_title",
+  "type": "text",
+  "human_label": "Job Title"
+}, {
+  "tag": "input",
+  "name": "date_of_birth",
+  "type": "date",
+  "human_label": "Date of Birth"
+}, {
+  "tag": "input",
+  "name": "parental_consent",
+  "type": "checkbox",
+  "human_label": "Parental Consent",
+	"conditional": {
+		"name": "date_of_birth",
+		"show_if": "(value) => {const now = new Date(); return value >= new Date(now.getFullYear() - 13, now.getMonth(), now.getDate());}"
+    }
+}
+]
+
+@app.route('/', methods=["GET"])
 def show_home_page():
-    """ Shows the homepage with links for login and registration.
+    """ Shows the homepage with form elements
     """
-    return render_template("homepage.html")
+    return render_template("homepage.html", fields=fields)
+
+
+@app.route('/register', methods=["POST"])
+def register_user_info():
+  """generates a clean json from user_info"""
+
+  info = {}
+
+  for field in fields:
+    info[field["name"]] = request.form.get(field["name"])
+
+  return jsonify(info)
 
 
 if __name__ == "__main__":
